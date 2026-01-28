3e54a0b8a50c67489f8135b1ecfaa12527780089
Knight <152365090+novaxmyth@users.noreply.github.com>
2026-01-28 16:11:32 +0630

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,22 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = f"start"
+        self.AuthorizeCommand = f"authorize"
+        self.UnAuthorizeCommand = f"unauthorize"
+        self.AddSudoCommand = f"addsudo"
+        self.RmSudoCommand = f"rmsudo"
+        self.PingCommand = f"ping"
+        self.RestartCommand = f"restart"
+        self.StatsCommand = f"stats"
+        self.HelpCommand = f"help"
+        self.LogCommand = f"log"
+        self.ShellCommand = f"shell"
+        self.AExecCommand = f"aexec"
+        self.ExecCommand = f"exec"
+        self.ClearLocalsCommand = f"clearlocals"
+        self.BotSetCommand = f"bsetting"
+        self.RssCommand = f"rss"
+        self.MediainfoCommand = f"mediainfo"
+
+
+BotCommands = _BotCommands()
---
+++ bot/helper/telegram_helper/message_utils.py
@@ -0,0 +1,70 @@
+from asyncio import sleep
+from pyrogram.errors import FloodWait
+from re import match as re_match
+from time import time
+from bot import config_dict, LOGGER, bot
+
+
+async def send_message(message, text, buttons=None, block=True):
+    try:
+        return await message.reply(
+            text=text,
+            quote=True,
+            disable_notification=True,
+            reply_markup=buttons,
+        )
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        if not block:
+            return str(f)
+        await sleep(f.value * 1.2)
+        return await send_message(message, text, buttons)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return str(e)
+
+
+async def edit_message(message, text, buttons=None, block=True):
+    try:
+        return await message.edit(
+            text=text,
+            reply_markup=buttons,
+        )
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        if not block:
+            return str(f)
+        await sleep(f.value * 1.2)
+        return await edit_message(message, text, buttons)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return str(e)
+
+
+async def send_file(message, file, caption=""):
+    try:
+        return await message.reply_document(
+            document=file, quote=True, caption=caption, disable_notification=True
+        )
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        await sleep(f.value * 1.2)
+        return await send_file(message, file, caption)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return str(e)
+
+
+async def delete_message(message):
+    try:
+        await message.delete()
+    except Exception as e:
+        LOGGER.error(str(e))
+
+
+async def auto_delete_message(cmd_message=None, bot_message=None):
+    await sleep(60)
+    if cmd_message is not None:
+        await deleteMessage(cmd_message)
+    if bot_message is not None:
+        await deleteMessage(bot_message)
